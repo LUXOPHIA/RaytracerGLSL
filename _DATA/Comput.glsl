7a0f42a42b0b3a257a9653a86ca362ea6c9182ce
luxidea <contact@luxidea.net>
2019-05-29 03:51:10 +0900

    no message

--- _DATA/Comput.glsl
+++ _DATA/Comput.glsl
@@ -115,11 +115,11 @@ void ObjPlane( in TRay Ray, inout THit Hit )
 {
   float t;
 
-  if( Ray.Vec.y < 0 )
+  if ( Ray.Vec.y < 0 )
   {
     t = ( Ray.Pos.y - -1.001 ) / -Ray.Vec.y;
 
-    if( ( 0 < t ) && ( t < Hit.t ) )
+    if ( ( 0 < t ) && ( t < Hit.t ) )
     {
       Hit.t   = t;
       Hit.Pos = Ray.Pos + t * Ray.Vec;
@@ -140,11 +140,11 @@ void ObjSpher( in TRay Ray, inout THit Hit )
 
   D = Pow2( B ) - C;
 
-  if( D > 0 )
+  if ( D > 0 )
   {
     t = -B - sign( C ) * sqrt( D );
 
-    if( ( 0 < t ) && ( t < Hit.t ) )
+    if ( ( 0 < t ) && ( t < Hit.t ) )
     {
       Hit.t   = t;
       Hit.Pos = Ray.Pos + t * Ray.Vec;
@@ -154,7 +154,7 @@ void ObjSpher( in TRay Ray, inout THit Hit )
   }
 }
 
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【材質】
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【質感】
 
 float _EmitShift = 0.0001;
 
@@ -167,7 +167,7 @@ TRay MatSkyer( in TRay Ray, in THit Hit )
   Result.Vec = Ray.Vec;
   Result.Pos = Ray.Pos;
   Result.Wei = Ray.Wei;
-  Result.Emi = texture( _Textur, VecToSky( Ray.Vec.xyz ) ).rgb;
+  Result.Emi = Ray.Emi + texture( _Textur, VecToSky( Ray.Vec.xyz ) ).rgb;
 
   return Result;
 }
@@ -181,7 +181,7 @@ TRay MatMirro( in TRay Ray, in THit Hit )
   Result.Vec = vec4( reflect( Ray.Vec.xyz, Hit.Nor.xyz ), 0 );
   Result.Pos = Hit.Pos + _EmitShift * Hit.Nor;
   Result.Wei = Ray.Wei;
-  Result.Emi = vec3( 0, 0, 0 );
+  Result.Emi = Ray.Emi;
 
   return Result;
 }
@@ -190,7 +190,9 @@ TRay MatMirro( in TRay Ray, in THit Hit )
 
 void Raytrace( inout TRay Ray )
 {
-  THit Hit = THit( 10000, 0, vec4( 0 ), vec4( 0 ) );
+  THit Hit;
+
+  Hit = THit( 10000, 0, vec4( 0 ), vec4( 0 ) );
 
   ///// 物体
 
@@ -212,7 +214,7 @@ void main()
 {
   vec4 E, S;
   TRay R;
-  vec3 A, B, C;
+  vec3 A, P;
 
   E = vec4( 0, 0, 0, 1 );
 
@@ -230,11 +232,9 @@ void main()
 
   A = R.Wei * R.Emi;
 
-  B = ToneMap( A, 10 );
+  P = GammaCorrect( ToneMap( A, 10 ), 2.2 );
 
-  C = GammaCorrect( B, 2.2 );
-
-  imageStore( _Imager, _WorkID.xy, vec4( C, 1 ) );
+  imageStore( _Imager, _WorkID.xy, vec4( P, 1 ) );
 }
 
 //############################################################################## ■
