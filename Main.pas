9f1b5ad1821b7d735fd6db28353c3f77abc7a0f6
luxidea <contact@luxidea.net>
2020-05-12 15:05:01 +0900

    コメントを記入

--- Main.pas
+++ Main.pas
@@ -22,38 +22,38 @@ uses
   LUX.GPU.OpenGL.Comput;
 
 type
-  TForm1 = class(TForm)
-    TabControl1: TTabControl;
-      TabItem1: TTabItem;
-        Image1: TImage;
-      TabItem2: TTabItem;
-        Memo1: TMemo;
-    Timer1: TTimer;
-    procedure FormCreate(Sender: TObject);
-    procedure FormDestroy(Sender: TObject);
-    procedure Image1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
-    procedure Image1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);
-    procedure Image1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
-    procedure Timer1Timer(Sender: TObject);
+  TForm1 = class(TForm)                                                         // アプリウィンドウクラスの宣言
+    TabControl1: TTabControl;                                                   // タブクラスのインスタ変数
+      TabItem1: TTabItem;                                                       // タブページクラスのインスタ変数
+        Image1: TImage;                                                         // 画像表示クラスのインスタ変数
+      TabItem2: TTabItem;                                                       // タブページクラスのインスタ変数
+        Memo1: TMemo;                                                           // テキスト表示クラスのインスタ変数
+    Timer1: TTimer;                                                             // 定期実行クラスのインスタ変数
+    procedure FormCreate(Sender: TObject);                                                               // アプリ起動イベントのメソッド
+    procedure FormDestroy(Sender: TObject);                                                              // アプリ終了イベントのメソッド
+    procedure Image1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);  // マウスボタンＯＮイベントのメソッド
+    procedure Image1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);                        // マウス移動イベントのメソッド
+    procedure Image1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);    // マウスボタンＯＦＦイベントのメソッド
+    procedure Timer1Timer(Sender: TObject);                                                              // 定義実行イベントのメソッド
   private
     { private 宣言 }
-    _MouseS :TShiftState;
-    _MouseP :TSingle2D;
-    _MouseA :TSingle2D;
+    _MouseS :TShiftState;                                                       // マウスボタンの状態集合の変数
+    _MouseP :TSingle2D;                                                         // マウスポインタの座標の変数
+    _MouseA :TSingle2D;                                                         // マウスポインタの累積座標の変数
   public
     { public 宣言 }
-    _ImageX :Integer;
-    _ImageY :Integer;
-    _Comput :TGLComput;
-    _Imager :TGLCelIma2D_TAlphaColorF;
-    _Camera :TGLStoBuf<TSingleM4>;
-    _Textur :TGLCelTex2D_TAlphaColorF;
+    _ImageX :Integer;                                                           // レンダリング画像の横ピクセル数の変数
+    _ImageY :Integer;                                                           // レンダリング画像の縦ピクセル数の変数
+    _Comput :TGLComput;                                                         // コンピュートシェーダクラスのインスタ変数
+    _Imager :TGLCelIma2D_TAlphaColorF;                                          // 書き換え可能なイメージクラスのインスタ変数
+    _Camera :TGLStoBuf<TSingleM4>;                                              // 行列配列クラスのインスタ変数
+    _Textur :TGLCelTex2D_TAlphaColorF;                                          // 線形補間可能なテクスチャクラスのインスタ変数
     ///// メソッド
-    procedure InitComput;
+    procedure InitComput;                                                       // コンピュートシェーダの初期化メソッド
   end;
 
 var
-  Form1: TForm1;
+  Form1: TForm1;                                                                // アプリウィンドウクラスのインスタ変数
 
 implementation //############################################################### ■
 
@@ -68,100 +68,100 @@ uses System.Math,
 
 /////////////////////////////////////////////////////////////////////// メソッド
 
-procedure TForm1.InitComput;
+procedure TForm1.InitComput;                                                    // コンピュートシェーダの初期化メソッド
 begin
-     _Comput.ItemsX := 10;
-     _Comput.ItemsY := 10;
-     _Comput.ItemsZ :=  1;
+     _Comput.ItemsX := 10;                                                      // スレッド数をＸ方向に１０個ずつの束にする
+     _Comput.ItemsY := 10;                                                      // スレッド数をＹ方向に１０個ずつの束にする
+     _Comput.ItemsZ :=  1;                                                      // スレッド数をＺ方向に　１個ずつの束にする
 
-     _Comput.WorksX := _ImageX;
-     _Comput.WorksY := _ImageY;
-     _Comput.WorksZ :=       1;
+     _Comput.WorksX := _ImageX;                                                 // Ｘ方向のスレッド数
+     _Comput.WorksY := _ImageY;                                                 // Ｙ方向のスレッド数
+     _Comput.WorksZ :=       1;                                                 // Ｚ方向のスレッド数
 
-     _Comput.ShaderC.Source.LoadFromFile( '..\..\_DATA\Comput.glsl' );
+     _Comput.ShaderC.Source.LoadFromFile( '..\..\_DATA\Comput.glsl' );          // コンピュートシェーダのソースを読み込む
 
-     with Memo1.Lines do
+     with Memo1.Lines do                                                        // Memo1 の Lines プロパティに対して･･･
      begin
-          Assign( _Comput.ShaderC.Errors );
+          Assign( _Comput.ShaderC.Errors );                                     // コンピュートシェーダからコンパイルメッセージをコピー
 
-          if Count > 0 then TabControl1.TabIndex := 1;
+          if Count > 0 then TabControl1.TabIndex := 1;                          // メッセージが存在する場合はタブページの２ページ目を表示
      end;
 
-     _Comput.Imagers.Add( '_Imager', _Imager );
-     _Comput.Buffers.Add( 'TCamera', _Camera );
-     _Comput.Texturs.Add( '_Textur', _Textur );
+     _Comput.Imagers.Add( '_Imager', _Imager );                                 // コンピュートシェーダにイメージクラスを登録
+     _Comput.Buffers.Add( 'TCamera', _Camera );                                 // コンピュートシェーダに行列配列クラスを登録
+     _Comput.Texturs.Add( '_Textur', _Textur );                                 // コンピュートシェーダにテクスチャクラスを登録
 end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-procedure TForm1.FormCreate(Sender: TObject);
+procedure TForm1.FormCreate(Sender: TObject);                                   // アプリ起動イベントのメソッド
 begin
-     Image1.AutoCapture := True;
+     Image1.AutoCapture := True;                                                // Image1 上でマウスポインタを常に補足
 
-     _ImageX := 800;
-     _ImageY := 600;
+     _ImageX := 800;                                                            // レンダリング画像の横ピクセル数を設定
+     _ImageY := 600;                                                            // レンダリング画像の縦ピクセル数を設定
 
-     _Comput := TGLComput               .Create;
-     _Imager := TGLCelIma2D_TAlphaColorF.Create;
-     _Camera := TGLStoBuf<TSingleM4>    .Create( GL_DYNAMIC_DRAW );
-     _Textur := TGLCelTex2D_TAlphaColorF.Create;
+     _Comput := TGLComput               .Create;                                // コンピュートシェーダの生成
+     _Imager := TGLCelIma2D_TAlphaColorF.Create;                                // イメージの生成
+     _Camera := TGLStoBuf<TSingleM4>    .Create( GL_DYNAMIC_DRAW );             // 行列配列の生成
+     _Textur := TGLCelTex2D_TAlphaColorF.Create;                                // テクスチャの生成
 
-     InitComput;
+     InitComput;                                                                // コンピュートシェーダの初期化
 
-     _Imager.Grid.CellsX := _ImageX;
-     _Imager.Grid.CellsY := _ImageY;
+     _Imager.Grid.CellsX := _ImageX;                                            // イメージの横ピクセル数を設定
+     _Imager.Grid.CellsY := _ImageY;                                            // イメージの横ピクセル数を設定
 
-     _Textur.Imager.LoadFromFileHDR( '..\..\_DATA\Luxo-Jr_2000x1000.hdr' );
+     _Textur.Imager.LoadFromFileHDR( '..\..\_DATA\Luxo-Jr_2000x1000.hdr' );     // テクスチャにＨＤＲＩを読み込む
 end;
 
-procedure TForm1.FormDestroy(Sender: TObject);
+procedure TForm1.FormDestroy(Sender: TObject);                                  // アプリ終了イベントのメソッド
 begin
-     _Comput.DisposeOf;
-     _Imager.DisposeOf;
-     _Camera.DisposeOf;
-     _Textur.DisposeOf;
+     _Comput.DisposeOf;                                                         // コンピュートシェーダの廃棄
+     _Imager.DisposeOf;                                                         // イメージの廃棄
+     _Camera.DisposeOf;                                                         // 行列配列の廃棄
+     _Textur.DisposeOf;                                                         // テクスチャの廃棄
 end;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-procedure TForm1.Timer1Timer(Sender: TObject);
+procedure TForm1.Timer1Timer(Sender: TObject);                                  // 定期実行クラスのメソッド
 begin
-     _Camera[ 0 ] := TSingleM4.RotateY( DegToRad( -_MouseA.X ) )
-                   * TSingleM4.RotateX( DegToRad( -_MouseA.Y ) )
-                   * TSingleM4.Translate( 0, 0, 3 );
+     _Camera[ 0 ] := TSingleM4.RotateY( DegToRad( -_MouseA.X ) )                //    Ｙ軸回転行列
+                   * TSingleM4.RotateX( DegToRad( -_MouseA.Y ) )                // × Ｘ軸回転行列
+                   * TSingleM4.Translate( 0, 0, 3 );                            // × 平行移動行列 を行列配列へ代入
 
-     _Comput.Run;
+     _Comput.Run;                                                               // コンピュートシェーダを実行
 
-     _Imager.CopyTo( Image1.Bitmap );
+     _Imager.CopyTo( Image1.Bitmap );                                           // イメージを画像表示ＵＩへコピー
 end;
 
 //------------------------------------------------------------------------------
 
-procedure TForm1.Image1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+procedure TForm1.Image1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);  // マウスボタンＯＮイベントのメソッド
 begin
-     _MouseS := Shift;
-     _MouseP := TSingle2D.Create( X, Y );
+     _MouseS := Shift;                                                          // マウスボタンの状態集合を取得
+     _MouseP := TSingle2D.Create( X, Y );                                       // マウスポインタ座標を取得
 end;
 
-procedure TForm1.Image1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);
+procedure TForm1.Image1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);  // マウス移動イベントのメソッド
 var
-   P :TSingle2D;
+   P :TSingle2D;                                                                // 現在のマウスポインタ座標の変数
 begin
-     if ssLeft in _MouseS then
+     if ssLeft in _MouseS then                                                  // マウスボタンの状態集合に左ボタンが含まれていた場合
      begin
-          P := TSingle2D.Create( X, Y );
+          P := TSingle2D.Create( X, Y );                                        // 現在のマウスポインタ座標を取得
 
-          _MouseA := _MouseA + ( P - _MouseP );
+          _MouseA := _MouseA + ( P - _MouseP );                                 // マウスポインタの移動差分を累積
 
-          _MouseP := P;
+          _MouseP := P;                                                         // 現在のマウスポインタ座標を過去の座標とする
      end;
 end;
 
-procedure TForm1.Image1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+procedure TForm1.Image1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);  // マウスボタンＯＦＦイベントのメソッド
 begin
-     Image1MouseMove( Sender, Shift, X, Y );
+     Image1MouseMove( Sender, Shift, X, Y );                                    // マウス移動イベントを実行
 
-     _MouseS := [];
+     _MouseS := [];                                                             // マウスボタンの状態集合の変数に空集合を代入
 end;
 
 end. //######################################################################### ■
--- _DATA/Comput.glsl
+++ _DATA/Comput.glsl
@@ -5,44 +5,44 @@
 //layout( local_size_variable ) in;
   layout( local_size_x = 10,
           local_size_y = 10,
-          local_size_z =  1 ) in;
+          local_size_z =  1 ) in;                                               // スレッドグループサイズの固定設定
 
 ////////////////////////////////////////////////////////////////////////////////
 
-  ivec3 _WorkGrupsN = ivec3( gl_NumWorkGroups );
+  ivec3 _WorkGrupsN = ivec3( gl_NumWorkGroups );                                // スレッドグループ数
 
 //ivec3 _WorkItemsN = ivec3( gl_LocalGroupSizeARB );
-  ivec3 _WorkItemsN = ivec3( gl_WorkGroupSize     );
+  ivec3 _WorkItemsN = ivec3( gl_WorkGroupSize     );                            // グループ内のスレッド数
 
-  ivec3 _WorksN     = _WorkGrupsN * _WorkItemsN;
+  ivec3 _WorksN     = _WorkGrupsN * _WorkItemsN;                                // 全スレッド数
 
-  ivec3 _WorkID     = ivec3( gl_GlobalInvocationID );
+  ivec3 _WorkID     = ivec3( gl_GlobalInvocationID );                           // 現スレッドの座標
 
 //############################################################################## ■
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
 
-const float Pi  = 3.141592653589793;
-const float Pi2 = Pi * 2.0;
-const float P2i = Pi / 2.0;
+const float Pi  = 3.141592653589793;                                            // π
+const float Pi2 = Pi * 2.0;                                                     // 2π
+const float P2i = Pi / 2.0;                                                     // π/2
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
 
-float Pow2( in float X )
+float Pow2( in float X )                                                        // 二乗関数
 {
   return X * X;
 }
 
 //------------------------------------------------------------------------------
 
-float length2( in vec3 V )
+float length2( in vec3 V )                                                      // ベクトル長の二乗関数
 {
   return Pow2( V.x ) + Pow2( V.y ) + Pow2( V.z );
 }
 
 //------------------------------------------------------------------------------
 
-vec2 VecToSky( in vec3 Vec )
+vec2 VecToSky( in vec3 Vec )                                                    // ベクトル→球面座標変換関数
 {
   vec2 Result;
 
@@ -54,14 +54,14 @@ vec2 VecToSky( in vec3 Vec )
 
 //------------------------------------------------------------------------------
 
-vec3 ToneMap( in vec3 Color, in float White )
+vec3 ToneMap( in vec3 Color, in float White )                                   // トーンマッピング関数
 {
   return clamp( Color * ( 1 + Color / White ) / ( 1 + Color ), 0, 1 );
 }
 
 //------------------------------------------------------------------------------
 
-vec3 GammaCorrect( in vec3 Color, in float Gamma )
+vec3 GammaCorrect( in vec3 Color, in float Gamma )                              // ガンマ補正関数
 {
   vec3 Result;
 
@@ -76,42 +76,42 @@ vec3 GammaCorrect( in vec3 Color, in float Gamma )
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【外部変数】
 
-writeonly uniform image2D _Imager;
+writeonly uniform image2D _Imager;                                              // イメージオブジェクトの宣言
 
-layout( std430 ) buffer TCamera
+layout( std430 ) buffer TCamera                                                 // SSBO の宣言
 {
-  layout( row_major ) mat4 _Camera;
+  layout( row_major ) mat4 _Camera;                                             // 4x4行列変数の宣言
 };
 
-uniform sampler2D _Textur;
+uniform sampler2D _Textur;                                                      // テクスチャオブジェクトの宣言
 
 //############################################################################## ■
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRay
 
-struct TRay
+struct TRay                                                                     // 光線の構造体宣言
 {
-  vec4 Pos;
-  vec4 Vec;
-  vec3 Wei;
-  vec3 Emi;
+  vec4 Pos;                                                                     // 出射点
+  vec4 Vec;                                                                     // 出射ベクトル
+  vec3 Wei;                                                                     // 重み
+  vec3 Emi;                                                                     // 流入輝度
 };
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% THit
 
-struct THit
+struct THit                                                                     // 光線衝突点の構造体宣言
 {
-  float t;
-  int   Mat;
-  vec4  Pos;
-  vec4  Nor;
+  float t;                                                                      // 光線の出射点から衝突点までの距離
+  int   Mat;                                                                    // 衝突点のマテリアルＩＤ
+  vec4  Pos;                                                                    // 衝突点の位置
+  vec4  Nor;                                                                    // 衝突点の法線
 };
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【内部変数】
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【物体】
 
-void ObjPlane( in TRay Ray, inout THit Hit )
+void ObjPlane( in TRay Ray, inout THit Hit )                                    // 無限平面と光線の交差判定関数
 {
   float t;
 
@@ -131,7 +131,7 @@ void ObjPlane( in TRay Ray, inout THit Hit )
 
 //------------------------------------------------------------------------------
 
-void ObjSpher( in TRay Ray, inout THit Hit )
+void ObjSpher( in TRay Ray, inout THit Hit )                                    // 球体と光線の交差判定関数
 {
   float B, C, D, t;
 
@@ -156,11 +156,11 @@ void ObjSpher( in TRay Ray, inout THit Hit )
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【材質】
 
-float _EmitShift = 0.0001;
+float _EmitShift = 0.0001;                                                      // 反射シフト量
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TRay MatSkyer( in TRay Ray, in THit Hit )
+TRay MatSkyer( in TRay Ray, in THit Hit )                                       // スカイドームのマテリアル関数
 {
   TRay Result;
 
@@ -174,7 +174,7 @@ TRay MatSkyer( in TRay Ray, in THit Hit )
 
 //------------------------------------------------------------------------------
 
-TRay MatMirro( in TRay Ray, in THit Hit )
+TRay MatMirro( in TRay Ray, in THit Hit )                                       // 鏡面反射のマテリアル関数
 {
   TRay Result;
 
@@ -188,23 +188,23 @@ TRay MatMirro( in TRay Ray, in THit Hit )
 
 //##############################################################################
 
-void Raytrace( inout TRay Ray )
+void Raytrace( inout TRay Ray )                                                 // レイトレース関数
 {
   THit Hit;
 
-  Hit = THit( 10000, 0, vec4( 0 ), vec4( 0 ) );
+  Hit = THit( 10000, 0, vec4( 0 ), vec4( 0 ) );                                 // 衝突点を無限遠に初期化
 
   ///// 物体
 
-  ObjSpher( Ray, Hit );
-  ObjPlane( Ray, Hit );
+  ObjSpher( Ray, Hit );                                                         // 球体と光線の交差判定
+  ObjPlane( Ray, Hit );                                                         // 平面と光線の交差判定
 
   ///// 材質
 
-  switch( Hit.Mat )
+  switch( Hit.Mat )                                                             // 衝突した材質に応じて光線の再生産過程を分岐
   {
-    case 0: Ray = MatSkyer( Ray, Hit ); return;
-    case 1: Ray = MatMirro( Ray, Hit ); break;
+    case 0: Ray = MatSkyer( Ray, Hit ); return;                                 // 空に当たったら終了
+    case 1: Ray = MatMirro( Ray, Hit ); break;                                  // 鏡面素材に当たったら光線を再生産
   }
 }
 
@@ -216,25 +216,25 @@ void main()
   TRay R;
   vec3 A, P;
 
-  E = vec4( 0, 0, 0, 1 );
+  E = vec4( 0, 0, 0, 1 );                                                       // 視点位置座標
 
-  S.x = 4.0 * ( _WorkID.x + 0.5 ) / _WorksN.x - 2.0;
+  S.x = 4.0 * ( _WorkID.x + 0.5 ) / _WorksN.x - 2.0;                            // スクリーン上のピクセル位置座標
   S.y = 1.5 - 3.0 * ( _WorkID.y + 0.5 ) / _WorksN.y;
   S.z = -2;
   S.w = 1;
 
-  R.Pos = _Camera * E;
-  R.Vec = _Camera * normalize( S - E );
-  R.Wei = vec3( 1 );
-  R.Emi = vec3( 0 );
+  R.Pos = _Camera * E;                                                          // 光線の出射点を視点
+  R.Vec = _Camera * normalize( S - E );                                         // スクリーンのピルセルに向かって光線を発射
+  R.Wei = vec3( 1 );                                                            // 初期ウェイトは１
+  R.Emi = vec3( 0 );                                                            // 初期輝度はゼロ
 
-  Raytrace( R );
+  Raytrace( R );                                                                // レイトレース
 
-  A = R.Wei * R.Emi;
+  A = R.Wei * R.Emi;                                                            // 最終的な流入輝度と光線のウェイトをかけ算
 
-  P = GammaCorrect( ToneMap( A, 10 ), 2.2 );
+  P = GammaCorrect( ToneMap( A, 10 ), 2.2 );                                    // トーンマッピングとガンマ補正
 
-  imageStore( _Imager, _WorkID.xy, vec4( P, 1 ) );
+  imageStore( _Imager, _WorkID.xy, vec4( P, 1 ) );                              // イメージオブジェクトに書き込み
 }
 
 //############################################################################## ■
