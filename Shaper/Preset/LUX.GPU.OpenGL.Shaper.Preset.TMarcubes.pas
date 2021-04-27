280610ca2bca2e9760029e087630e76ee0ffa77d
luxidea <contact@luxidea.net>
2021-04-27 20:49:34 +0900

    Squashed '_LIBRARY/LUXOPHIA/LUX.GPU.OpenGL/' changes from 6425737..11770d9
    11770d9 DisposeOf → Free
    8c26828 LUX.M4 → LUX.D4x4 ユニットへ置換。
    956e3ff .DisposeOf → .Free へ置換。
    
    git-subtree-dir: _LIBRARY/LUXOPHIA/LUX.GPU.OpenGL
    git-subtree-split: 11770d9d3751211a433957d4bdb53371190da2cf

--- Shaper/Preset/LUX.GPU.OpenGL.Shaper.Preset.TMarcubes.pas
+++ Shaper/Preset/LUX.GPU.OpenGL.Shaper.Preset.TMarcubes.pas
@@ -3,7 +3,7 @@
 interface //#################################################################### ■
 
 uses Winapi.OpenGL, Winapi.OpenGLext,
-     LUX, LUX.D2, LUX.D3, LUX.M4,
+     LUX, LUX.D2, LUX.D3, LUX.D4x4,
      LUX.GPU.OpenGL,
      LUX.GPU.OpenGL.Atom.Buffer.UniBuf,
      LUX.GPU.OpenGL.Atom.Textur.D2.Preset,
@@ -223,7 +223,7 @@ end;
 
 destructor TMarcubesMateryFacesMIR.Destroy;
 begin
-     _Textur.DisposeOf;
+     _Textur.Free;
 
      inherited;
 end;
@@ -375,9 +375,9 @@ end;
 
 destructor TMarcubes.Destroy;
 begin
-     _Textur   .DisposeOf;
-     _Size     .DisposeOf;
-     _Threshold.DisposeOf;
+     _Textur   .Free;
+     _Size     .Free;
+     _Threshold.Free;
 
      inherited;
 end;
@@ -442,7 +442,7 @@ begin
         MakeModel;
 
      finally
-            F.DisposeOf;
+            F.Free;
      end;
 end;
 
